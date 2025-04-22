9f308cc7143cd921d9ec8c76d07bea6cc4695dc8
Mariano Aluzzo <mariano.aluzzo@alusoft.com.ar>
2025-04-21 22:58:11 -0300

    Correccion carga de asientos

--- ERP_Alusoft/Model/LineaAsiento.cs
+++ ERP_Alusoft/Model/LineaAsiento.cs
@@ -1,13 +1,26 @@
-﻿namespace ERP_Alusoft.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ERP_Alusoft.Model
 {
     public class LineaAsiento
     {
         public int Id { get; set; }
         public int Asiento_Id { get; set; }
+
+
+        [Required(ErrorMessage = "Debe seleccionar una cuenta")]
         public Nullable<int> Plan_CuentasId { get; set; }
         public string Concepto { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El campo Debe es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Debe ser mayor que 0")]
         public Nullable<decimal> Debe { get; set; }
+
+        [Required(ErrorMessage = "El campo Haber es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Debe ser mayor que 0")]
         public Nullable<decimal> Haber { get; set; }
+
+
         public Plan_Cuentas Plan_Cuentas { get; set; }        
 
     }
